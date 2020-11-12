67d109bbcefdc6b8045448b5460e09cd706918ff
Jakob Bøgh Larsen <jbla20@student.aau.dk>
2020-11-12 13:26:52 +0100

    Fixed desired orientation

--- src/path_basis.cpp
+++ src/path_basis.cpp
@@ -5,9 +5,7 @@
 #include <iostream>
 #include <turtlesim/Pose.h>
 
-
 using namespace std;
-
 
 ros::Publisher vel_pub;
 ros::Subscriber sub_pose;
@@ -16,7 +14,7 @@ ros::Subscriber sub_pose;
 
 void move(double speed, double distance, bool isForward);
 void rotate(double angular_speed, double angle, bool clockwise);
-void poseCallback(const turtlesim::Pose::ConstPtr& pose_message);
+void poseCallback(const turtlesim::Pose::ConstPtr &pose_message);
 double euclidean_distance(double x1, double y1, double x2, double y2);
 double linear_velocity(turtlesim::Pose goal);
 double angular_velocity(turtlesim::Pose goal);
@@ -32,17 +30,14 @@ const double pi = M_PI;
 
 turtlesim::Pose cur_pose;
 
-
 int main(int argc, char *argv[])
-{   
-    ros::init(argc,argv, "robot_cleaner");
+{
+    ros::init(argc, argv, "robot_cleaner");
     ros::NodeHandle n;
 
     double speed, angular_speed;
     double distance, angle;
     bool isForward, clockwise;
-
-
 
     vel_pub = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 10);
     sub_pose = n.subscribe("/turtle1/pose", 10, &poseCallback);
@@ -55,7 +50,7 @@ int main(int argc, char *argv[])
     // cout << "Enter distance: ";
     // cin >> distance;
     // cout << "Forward?: ";
-    // cin >> isForward;   
+    // cin >> isForward;
 
     // move(speed, distance, isForward);
 
@@ -64,8 +59,7 @@ int main(int argc, char *argv[])
     // cout << "Enter angle: ";
     // cin >> angle;
     // cout << "Clockwise?: ";
-    // cin >> clockwise;   
-
+    // cin >> clockwise;
 
     // rotate(angular_speed,angle,clockwise);
 
@@ -76,37 +70,38 @@ int main(int argc, char *argv[])
     // std::cin >> goal_pose.y;
     // std::cout << "Set your theta goal: ";
     // std::cin >> goal_pose.theta;
-    // goal_pose.x = 1;
-    // goal_pose.y = 3;
+    goal_pose.x = 1;
+    goal_pose.y = 3;
     //rotate(1,M_PI,0);
     //setDesiredOrientation(M_PI + M_PI_2);
-        //degrees2radians(90);
-    for (int i = 1; i < 11; i++)
-    {
-        if(i%2 == 1){
-            for (int j = 1; j < 11; j++)
-            {
-                goal_pose.x = i;
-                goal_pose.y = j;
-                move2goal(goal_pose);
-            }
-        }
-        else {
-            for (int j = 10; j > 0; j--)
-            {
-                goal_pose.x = i;
-                goal_pose.y = j;
-                move2goal(goal_pose);
-            }
-            
-        }
-        
-    }
-    
-    
+    //degrees2radians(90);
+    // for (int i = 1; i < 11; i++)
+    // {
+    //     if(i%2 == 1){
+    //         for (int j = 1; j < 11; j++)
+    //         {
+    //             goal_pose.x = i;
+    //             goal_pose.y = j;
+    //             move2goal(goal_pose);
+    //         }
+    //     }
+    //     else {
+    //         for (int j = 10; j > 0; j--)
+    //         {
+    //             goal_pose.x = i;
+    //             goal_pose.y = j;
+    //             move2goal(goal_pose);
+    //         }
+
+    //     }
+
+    // }
+
+    move2goal(goal_pose);
+    setDesiredOrientation(M_PI_2);
+
     //cout << "done";
     //goal_pose.theta = 2 * M_PI;
-    
 
     return 0;
 }
@@ -116,10 +111,11 @@ int main(int argc, char *argv[])
  * certain distance in a forward or backward straight direction.
  * */
 
-void move(double speed, double distance, bool isForward){
+void move(double speed, double distance, bool isForward)
+{
     geometry_msgs::Twist vel_msg;
 
-    if(isForward)
+    if (isForward)
         vel_msg.linear.x = abs(speed);
     else
     {
@@ -136,24 +132,24 @@ void move(double speed, double distance, bool isForward){
     double current_distance = 0.0;
 
     ros::Rate loop_rate(100);
-    do{
+    do
+    {
         vel_pub.publish(vel_msg);
         double t1 = ros::Time::now().toSec();
-        current_distance = speed * (t1-t0);
+        current_distance = speed * (t1 - t0);
         ros::spinOnce();
         loop_rate.sleep();
 
-    }while(current_distance<distance);
+    } while (current_distance < distance);
     vel_msg.linear.x = 0;
     vel_pub.publish(vel_msg);
-    
+
     //distance = speed * time
-
-
 }
 
 //positive angular speed rotation is counteclockwise.
-void rotate(double angular_speed, double angle, bool clockwise){
+void rotate(double angular_speed, double angle, bool clockwise)
+{
     geometry_msgs::Twist vel_msg;
     vel_msg.linear.x = 0;
     vel_msg.linear.y = 0;
@@ -161,76 +157,78 @@ void rotate(double angular_speed, double angle, bool clockwise){
 
     vel_msg.angular.x = 0;
     vel_msg.angular.y = 0;
-    
-    if(clockwise){
+
+    if (clockwise)
+    {
         vel_msg.angular.z = -abs(angular_speed);
     }
-    else{
+    else
+    {
         vel_msg.angular.z = abs(angular_speed);
     }
-    
+
     double current_angle = cur_pose.theta;
     double t0 = ros::Time::now().toSec();
     ros::Rate loop_rate(10);
-    do{
+    do
+    {
         vel_pub.publish(vel_msg);
         double t1 = ros::Time::now().toSec();
-        current_angle = angular_speed * (t1-t0);
+        current_angle = angular_speed * (t1 - t0);
         ros::spinOnce();
-    }while(current_angle<=angle);
+    } while (current_angle <= angle);
     vel_msg.angular.z = 0;
     vel_pub.publish(vel_msg);
 }
 
-void poseCallback(const turtlesim::Pose::ConstPtr& pose_message)
+void poseCallback(const turtlesim::Pose::ConstPtr &pose_message)
 {
-    
 
-    cur_pose.x = pose_message ->x;
-    cur_pose.y = pose_message ->y;
-    cur_pose.theta = pose_message -> theta;
+    cur_pose.x = pose_message->x;
+    cur_pose.y = pose_message->y;
+    cur_pose.theta = pose_message->theta;
     //ROS_INFO_STREAM("position=(" << cur_pose.x << "," << cur_pose.y << ")" << " angle= " << cur_pose.theta );
 
     //std::cout << "x: " << cur_pose.x << std::endl << "y: " << cur_pose.y << std::endl << "theta: " << cur_pose.theta << std::endl;
-
-    
 
     // cur_x = pose_message.x;
     // cur_y = pose_message.y;
     // cur_theta = pose_message.theta;
 }
 
-double euclidean_distance(double x1, double y1, double x2, double y2){
+double euclidean_distance(double x1, double y1, double x2, double y2)
+{
     return sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
 }
 
-double linear_velocity(turtlesim::Pose goal){
+double linear_velocity(turtlesim::Pose goal)
+{
     double kv = 1;
 
     double distance = euclidean_distance(cur_pose.x, cur_pose.y, goal.x, goal.y);
-    
+
     //cout << "Distance: " << distance << endl;
 
     return kv * distance;
-    
 }
 
-double angular_velocity(turtlesim::Pose goal){
+double angular_velocity(turtlesim::Pose goal)
+{
     double ka = 4;
 
     return ka * (atan2(goal.y - cur_pose.y, goal.x - cur_pose.x) - cur_pose.theta);
 }
 
-void move2goal(turtlesim::Pose goal){
-    
+void move2goal(turtlesim::Pose goal)
+{
 
     geometry_msgs::Twist vel_msg;
     ros::Rate loop_rate = (10);
 
-    
-    while(euclidean_distance(cur_pose.x, cur_pose.y, goal.x, goal.y) > distance_tolerance){
+    while (euclidean_distance(cur_pose.x, cur_pose.y, goal.x, goal.y) > distance_tolerance)
+    {
         // std::cout << "x: " << cur_pose.x << std::endl << "y: " << cur_pose.y << std::endl << "theta: " << cur_pose.theta << std::endl;
-        
+
         vel_msg.linear.x = linear_velocity(goal);
         vel_msg.linear.y = 0;
         vel_msg.linear.z = 0;
@@ -242,9 +240,10 @@ void move2goal(turtlesim::Pose goal){
         vel_pub.publish(vel_msg);
 
         //cout << "still looping" << endl;
-        if(euclidean_distance(cur_pose.x, cur_pose.y, goal.x, goal.y) > distance_tolerance){
-        loop_rate.sleep();
-        ros::spinOnce();
+        if (euclidean_distance(cur_pose.x, cur_pose.y, goal.x, goal.y) > distance_tolerance)
+        {
+            loop_rate.sleep();
+            ros::spinOnce();
         }
     }
     vel_msg.linear.x = 0;
@@ -253,14 +252,15 @@ void move2goal(turtlesim::Pose goal){
 }
 
 void setDesiredOrientation(double desired_angle_radians)
-{	
-	double relative_angle_radians = desired_angle_radians - cur_pose.theta;
-	//if we want to turn at a perticular orientation, we subtract the current orientation from it
-	bool clockwise = ((relative_angle_radians<0)?true:false);
-	cout<<desired_angle_radians <<","<<cur_pose.theta<<","<<relative_angle_radians<<","<<clockwise<<endl;
-	rotate (2, abs(relative_angle_radians), clockwise);
+{
+    double relative_angle_radians = desired_angle_radians - cur_pose.theta;
+    //if we want to turn at a perticular orientation, we subtract the current orientation from it
+    bool clockwise = ((relative_angle_radians < 0) ? true : false);
+    cout << desired_angle_radians << "," << cur_pose.theta << "," << relative_angle_radians << "," << clockwise << endl;
+    rotate(2, abs(relative_angle_radians), clockwise);
 }
 
-double degrees2radians(double angle_in_degrees){
-	return angle_in_degrees * M_PI /180.0;
+double degrees2radians(double angle_in_degrees)
+{
+    return angle_in_degrees * M_PI / 180.0;
 }
