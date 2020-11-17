358c39f7fa3f6d9ae1b271aa08212330f33f9472
Victor Risager <vrisag20@student.aau.dk>
2020-11-17 19:20:37 +0100

    Added conversion from quaterion to euler angles, to be able to have radian angles around the z-axis.

--- src/gazebo_odom.cpp
+++ src/gazebo_odom.cpp
@@ -5,9 +5,69 @@
 
 ros::Subscriber sub_pose;
 
-void poseCallback(const nav_msgs::Odometry::ConstPtr &pose_message){
-    ROS_INFO_STREAM("Angular: " << pose_message->twist.twist.angular.z << ", Linear: " << pose_message->twist.twist.linear.x );
+
+
+#pragma region Quaterion To Euler Angles conversion
+struct Quaternion{
+    double x, y, z, w;
+};
+
+struct EulerAngles {
+    double roll, pitch, yaw;
+};
+
+
+//convert quarternion into eulerangles.
+EulerAngles ToEulerAngles(Quaternion q){
+    EulerAngles angles;
+
+    //roll (x axis rotation)
+    double sinr_cosp = 2 * (q.w * q.x - q.y * q.z);
+    double cosr_cosp = 1 - 2 * (q.x * q.x + q.y * q.y);
+    angles.roll = atan2(sinr_cosp, cosr_cosp);
+
+    //pitch (y axis rotation)
+    double sinp = 2 * (q.w * q.y - q.z * q.x);
+    if (abs(sinp) >= 1)
+        //copysign returns the magnitude of M_PI/2 with the sign of sinp
+        angles.pitch = copysign(M_PI / 2, sinp); // use 90 degrees if out of range
+    else
+        angles.pitch = asin(sinp);
+
+    // yaw (z-axis rotation)
+    double siny_cosp = 2 * (q.w * q.z + q.x * q.y);
+    double cosy_cosp = 1 - 2 * (q.y * q.y + q.z * q.z);
+    angles.yaw = std::atan2(siny_cosp, cosy_cosp);
+
+    return angles;
 }
+#pragma endregion
+
+
+void poseCallback(const nav_msgs::Odometry::ConstPtr &pose_message){
+    //ROS_INFO_STREAM("Angular: " << pose_message->twist.twist.angular.z << ", Linear: " << pose_message->twist.twist.linear.x );
+    
+    Quaternion q;
+
+    //assign values of pose message to quaternion.
+    q.x = pose_message->pose.pose.orientation.x;
+    q.y = pose_message->pose.pose.orientation.y;
+    q.z = pose_message->pose.pose.orientation.z;
+    q.w = pose_message->pose.pose.orientation.w;
+
+
+    double yaw = ToEulerAngles(q).yaw + M_PI;
+    double pitch = ToEulerAngles(q).pitch;
+    double roll = ToEulerAngles(q).roll;
+
+    std::cout << "yaw: " << yaw << std::endl;
+    std::cout << "pitch: " << pitch << std::endl;
+    std::cout << "roll: " << roll << std::endl << std::endl;
+
+}
+
+
+
 
 int main(int argc, char *argv[])
 {
@@ -20,3 +80,5 @@ int main(int argc, char *argv[])
         ros::spinOnce();
     }
 }
+
+
