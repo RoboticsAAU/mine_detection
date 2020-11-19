64c88a81cf8c7bc74900651fabf9e4b4f0e58698
Robotics Student 2020 <jolars20@student.aau.dk>
2020-11-19 14:42:14 +0100

    Added way to load images. noting works yet

--- src/detection.cpp
+++ src/detection.cpp
@@ -1,18 +1,48 @@
 #include <iostream>
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/core.hpp>
 
 using namespace cv;
 using namespace std;
 
 int main (int argc, char** argv){
-    VideoCapture cap(0); //Capture the video from webcam
+    int input = 0;
+    cout << "Do you want to detect using the Camera, press 1 or load in a image, press 2,\n";
+    cin >> input;
+    if (input == 2)
+        {
+        
+        // Hvis vi skal teste programmet uden selve kameraet at et billede føres ind ved brug af denne kommando
+        Mat img = imread("D:/My OpenCV Website/Lady with a Guitar.jpg", IMREAD_COLOR);
+            if(img.empty())
+            {
+            std::cout << "Could not read the image: " << std::endl;
+            return 1;
+            }
+        }
 
-    if( !cap.isOpened() ){ //if not successful program will shut down
-    cout << "Cannot open the webcam" << endl;
-    return -1;
-    }  
+    else 
+        {
+            VideoCapture cap(0); //Capture the video from webcam
+            if( !cap.isOpened() ) //if not successful program will shut down
+            {
+            cout << "Cannot open the webcam" << endl;
+            return -1;
+            } 
 
+            while(true)
+            {
+                bool bSuccess = cap.read(imgOriginal); //reads a new frame from camera
+                if (!bSuccess)//if image cant be read, break the loop 
+                { 
+                cout << "Cannot read frame from video stream" << endl;
+                break;
+                }
+            }
+        }
     namedWindow("control", CV_WINDOW_AUTOSIZE); //creates a windows called "Control"
 
     int iLowH = 0;
@@ -43,15 +73,7 @@ int main (int argc, char** argv){
 
     while (true)
     {
-        Mat imgOriginal; //make an ampty array
-
-        bool bSuccess = cap.read(imgOriginal); //reads a new frame from camera and puts it into imgOriginal
-
-        if (!bSuccess) { //if image cant be read, break the loop
-        cout << "Cannot read frame from video stream" << endl;
-        break;
-        }
-
+        Mat imgOriginal = Mat img;
         Mat imgHSV; //make an empty array
 
         cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //converts RGB colour to Hue, Saturation, Value and puts it intp imgHSV
