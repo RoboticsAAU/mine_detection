2ea8a1c9952ce56131f30dba0555d91876186e5a
Victor Risager <victor.risager@hotmail.com>
2020-11-23 15:09:12 +0100

    Created the zigzag pattern in the point generation.

--- src/points_gen.cpp
+++ src/points_gen.cpp
@@ -1,6 +1,7 @@
 #include "points_gen.h"
 #include <vector>
 #include <iostream>
+#include <cmath>
 
 using namespace Points_gen;
 
@@ -12,16 +13,29 @@ std::vector<Point> points_List::gen_Point_list(Point* p){
     double m = 5.0;
     double n = 5.0;
     double robot_radius = 0.175;
+    int count = 0;
     
     for (double i = robot_radius; i < m; i+=2*robot_radius)
     {
-        //std::cout << i << std::endl;
-         for(double j = robot_radius; j < n; j += 2*robot_radius){
-            Point p = {i,j};
-            std::cout << p.x << "," << p.y << std::endl;
-            //std::cin.get();
-            vec.push_back(p);
-        } 
+        //std::cout << count % 2 << std::endl;
+        //std::cout << fmod(i,robot_radius) << std::endl;
+        if (count%2==0){
+            
+            for(double j = robot_radius; j < n; j += 2*robot_radius){
+                Point p = {i,j};
+                std::cout << p.x << "," << p.y << std::endl;
+                //std::cin.get();
+                vec.push_back(p);
+            } 
+        }else{
+            for(double j = n-robot_radius; j > 0; j -= 2*robot_radius){
+                Point p = {i,j};
+                std::cout << p.x << "," << p.y << std::endl;
+                //std::cin.get();
+                vec.push_back(p);
+            } 
+        }
+        count++;
     }
     
     std::cin.get();
