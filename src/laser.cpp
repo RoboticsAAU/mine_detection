d4726fcaffdd0f453955ad43041b5dd14c343d3c
Victor Risager <victor.risager@hotmail.com>
2020-11-26 15:05:14 +0100

    Created a function to determine a circular obstacles center and radius.

--- src/laser.cpp
+++ src/laser.cpp
@@ -21,26 +21,60 @@ void laserCallback(const sensor_msgs::LaserScan::ConstPtr &laser_msg)
 {
     float angle;
     int count = 0;
-    std::cout << "New array:" << std::endl;
+    points.clear();
+    points.shrink_to_fit();
+    //std::cout << "New array:" << std::endl;
     for (int i = 0; i < laser_msg->ranges.size(); i++)
     {
         angle = laser_msg->angle_min + (i * laser_msg->angle_increment);
         if (!std::isnan(laser_msg->ranges.at(i)))
         {
-            if (0.5 < laser_msg->ranges[i] && laser_msg->ranges[i] < 1.5)
+            if (0.5 < laser_msg->ranges[i] && laser_msg->ranges[i] < 1)
             {
-                //count++;
                 //std::cout << laser_msg->ranges.at(i) << std::endl;
                 Point p;
+                //calculate the cartesian coordinates.
                 p.x = laser_msg->ranges[i] * cos(angle);
                 p.y = laser_msg->ranges[i] * sin(angle);
-
-                std::cout << p.x << " : " << p.y << std::endl;
+                //std::cout << p.x << " : " << p.y << std::endl;
                 points.push_back(p);
             }
         }
     }
-    //std::cout << laser_msg->ranges.size() << ", count is: " << count << std::endl;
+    //std::cout << "Point count is: " << points.size() << std::endl;
+}
+
+Point getCenterOfCircle(std::vector<Point> *points)
+{
+    Point f = (*points).at(0);
+    Point m = (*points).at(points->size() / 2);
+    Point l = (*points).at(points->size() - 1);
+
+    // std::cout << f.x << " : " << f.y << std::endl;
+    // std::cout << m.x << " : " << m.y << std::endl;
+    // std::cout << l.x << " : " << l.y << std::endl;
+
+    //calculate determinants
+    double A = f.x * (m.y - l.y) - f.y * (m.x - l.x) + m.x * l.y - l.x * m.y;
+    double B = (pow(f.x, 2) + pow(f.y, 2)) * (l.y - m.y) + (pow(m.x, 2) + pow(m.y, 2)) * (f.y - l.y) + (pow(l.x, 2) + pow(l.y, 2)) * (m.y - f.y);
+    double C = (pow(f.x, 2) + pow(f.y, 2)) * (m.x - l.x) + (pow(m.x, 2) + pow(m.y, 2)) * (l.x - f.x) + (pow(l.x, 2) + pow(l.y, 2)) * (f.x - m.x);
+
+    // std::cout << "A : " << A << std::endl;
+    // std::cout << "B : " << B << std::endl;
+    // std::cout << "C : " << C << std::endl;
+
+    //Create center point.
+    Point center;
+    center.x = -B / (2 * A);
+    center.y = -C / (2 * A);
+
+    //return
+    return center;
+}
+
+double obstacleRadius(Point center, Point per_coordinate)
+{
+    return sqrt(pow(center.x - per_coordinate.x, 2) + pow(center.y - per_coordinate.y, 2));
 }
 
 int main(int argc, char *argv[])
@@ -50,10 +84,19 @@ int main(int argc, char *argv[])
 
     laser_sub = n.subscribe<sensor_msgs::LaserScan>("/scan", 10, &laserCallback);
 
-    ros::Rate loop_rate(0.5);
+    ros::Rate loop_rate(10);
+    Point center;
     while (ros::ok())
     {
         ros::spinOnce();
+        if (points.size() > 3)
+        {
+            center = getCenterOfCircle(&points);
+            double radius = obstacleRadius(center, points[0]);
+            std::cout << "Center: " << center.x << " : " << center.y << std::endl;
+            std::cout << "Radius: " << radius << std::endl;
+            std::cout << std::endl;
+        }
         loop_rate.sleep();
     }
     return 0;
