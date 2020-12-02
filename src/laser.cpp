166b4bb9ca2514e8977dcaa98202bcf9ced0e818
Victor Risager <victor.risager@hotmail.com>
2020-12-02 09:45:00 +0100

    Created a small publisher to test the message.

--- src/laser.cpp
+++ src/laser.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <vector>
 #include <math.h>
+#include "mine_detection/Obstacle.h"
 
 //#include "obstacle.h"
 
@@ -16,7 +17,7 @@ std::vector<Point> points;
 Point laser_offset = {0.08, 0.025};
 
 ros::Subscriber laser_sub;
-ros::Publisher laser_pub;
+ros::Publisher obstacle_pub;
 
 void laserCallback(const sensor_msgs::LaserScan::ConstPtr &laser_msg)
 {
@@ -73,6 +74,7 @@ Point getCenterOfCircle(std::vector<Point> *points)
     return center;
 }
 
+//calculate radius from center to peripheral point.
 double obstacleRadius(Point center, Point per_coordinate)
 {
     return sqrt(pow(center.x - per_coordinate.x, 2) + pow(center.y - per_coordinate.y, 2));
@@ -84,9 +86,11 @@ int main(int argc, char *argv[])
     ros::NodeHandle n;
 
     laser_sub = n.subscribe<sensor_msgs::LaserScan>("/scan", 10, &laserCallback);
+    obstacle_pub = n.advertise<mine_detection::Obstacle>("/obstacle", 10);
 
     ros::Rate loop_rate(10);
     Point center;
+    mine_detection::Obstacle obstacle_msg;
     while (ros::ok())
     {
         ros::spinOnce();
@@ -94,9 +98,16 @@ int main(int argc, char *argv[])
         {
             center = getCenterOfCircle(&points);
             double radius = obstacleRadius(center, points[0]);
+
+            obstacle_msg.x = center.x;
+            obstacle_msg.y = center.y;
+            obstacle_msg.r = radius;
+
             std::cout << "Center: " << center.x << " : " << center.y << std::endl;
             std::cout << "Radius: " << radius << std::endl;
             std::cout << std::endl;
+
+            obstacle_pub.publish(obstacle_msg);
         }
         loop_rate.sleep();
     }
