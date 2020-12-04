6143fe6aec2c2691f61b695440f8f2bc8fb53e73
Victor Risager <victor.risager@hotmail.com>
2020-12-04 13:26:14 +0100

    changed point publish header id, to /odom, and formatted paper_detection.cpp

--- src/paper_detection.cpp
+++ src/paper_detection.cpp
@@ -38,8 +38,8 @@ int main(int argc, char **argv)
      vector<bool> shouldPublish;
      int boundColour[] = {0, 0, 255};
      int contourColour[] = {0, 255, 0};
-     int surflimit = 250; //surflimit defines the lower boundary, where an object will be countoured and for which a bounding box will be made
-     int upperLimitOfdecrease = 100; // this defines the upper limit for the change of the size of the bounding boxes 
+     int surflimit = 250;            //surflimit defines the lower boundary, where an object will be countoured and for which a bounding box will be made
+     int upperLimitOfdecrease = 100; // this defines the upper limit for the change of the size of the bounding boxes
      int surfacedif;
 
      ros::init(argc, argv, "paper_detector");
@@ -47,7 +47,7 @@ int main(int argc, char **argv)
      point_pub = n.advertise<visualization_msgs::Marker>("/visualization_marker", 100); //visualization_msgs::Marker /visualization_marker
      sub_pose = n.subscribe("/turtle1/pose", 10, &poseCallback);
 
-     cv::VideoCapture cap(0); //Capture the video from webcam. 
+     cv::VideoCapture cap(0); //Capture the video from webcam.
      //If the webcam cannot open, it is likely due to the iindex is wrong, thus it is trying to open a webcam that is not accessible through that index.
 
      if (!cap.isOpened()) //If not success, exit program.
@@ -101,7 +101,6 @@ int main(int argc, char **argv)
           //Morphological closing (removes small holes from the foreground).
           dilate(imgThresholded, imgThresholded, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
           erode(imgThresholded, imgThresholded, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
-          
 
           vector<vector<cv::Point>> contours; // makes a 2D vector containing points
 
@@ -109,38 +108,39 @@ int main(int argc, char **argv)
 
           vector<cv::Rect> boundbox(contours.size());
           vector<vector<cv::Point>> contours_poly(contours.size());
-                    
-          for (size_t i = 0; i < contours.size(); i++)
-          {
-               
-               approxPolyDP(cv::Mat(contours[i]), contours_poly[i], 3, true);                         
-               boundbox[i] = boundingRect(contours_poly[i]);
-          }           
 
           for (size_t i = 0; i < contours.size(); i++)
-          {         
-               drawContours(imgOriginal, contours_poly, -1, (contourColour[0], contourColour[1], contourColour[2]), 3);      
-               rectangle(imgOriginal, boundbox[i].tl(), boundbox[i].br(), (boundColour[0], boundColour[1], boundColour[2]), 2, 8, 0);    
+          {
+
+               approxPolyDP(cv::Mat(contours[i]), contours_poly[i], 3, true);
+               boundbox[i] = boundingRect(contours_poly[i]);
+          }
+
+          for (size_t i = 0; i < contours.size(); i++)
+          {
+               drawContours(imgOriginal, contours_poly, -1, (contourColour[0], contourColour[1], contourColour[2]), 3);
+               rectangle(imgOriginal, boundbox[i].tl(), boundbox[i].br(), (boundColour[0], boundColour[1], boundColour[2]), 2, 8, 0);
                //std::cout << boundbox[i].tl() << boundbox[i].br() <<  std::endl;
           }
 
           imshow("Thresholded Image", imgThresholded); //show the thresholded image
           imshow("Original", imgOriginal);             //show the original image
-               
+
           vector<cv::Point> rectCenter(boundbox.size()); //current boundingbox center coordinates
           shouldPublish.resize(rectCenter.size(), true);
           //for (int j = 0; j =< rectCenter.size(); j++)
           //cout << " "
-          vector<int> rectSurface(boundbox.size());     //surface area of boundingbox last frame
-          lastRectSurface.resize(boundbox.size()); //surface area of boundingbox last frame
+          vector<int> rectSurface(boundbox.size()); //surface area of boundingbox last frame
+          lastRectSurface.resize(boundbox.size());  //surface area of boundingbox last frame
 
-          for (size_t i = 0; i < boundbox.size(); i++ ) 
-          {                                  
+          for (size_t i = 0; i < boundbox.size(); i++)
+          {
                rectSurface[i] = boundbox[i].width * boundbox[i].height;
                surfacedif = lastRectSurface[i] - rectSurface[i];
-               if (surfacedif > surflimit)  //if bounding rectangle is smaller than last frame save coordinated of bounding rectangle
-               {     
-                    rectCenter[i] = {boundbox[i].x + (boundbox[i].width/2), boundbox[i].y + (boundbox[i].height/2)};;
+               if (surfacedif > surflimit) //if bounding rectangle is smaller than last frame save coordinated of bounding rectangle
+               {
+                    rectCenter[i] = {boundbox[i].x + (boundbox[i].width / 2), boundbox[i].y + (boundbox[i].height / 2)};
+                    ;
                     for (size_t i = 0; i < rectCenter.size(); i++)
                     {
                          point centerCoord;
@@ -157,26 +157,24 @@ int main(int argc, char **argv)
                               shouldPublish[i] = false;
                          }
                     }
-               } 
+               }
                //if(i > 2 && rectsurf[i-2]<rectsurf[i-1]>rectsurf[i])
                /*if (surfacedif < surflimit)
                {
                shouldPublish[i] = false;
                }*/
-               lastRectSurface[i] = rectSurface[i]; 
+               lastRectSurface[i] = rectSurface[i];
           }
 
-               if (rectCenter.size() != 0) 
-               {
-                    
-               } 
+          if (rectCenter.size() != 0)
+          {
+          }
 
           if (cv::waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
           {
                std::cout << "esc key is pressed by user" << endl;
                break;
           }
-
      }
      return 0;
 }
@@ -191,10 +189,10 @@ void poseCallback(const turtlesim::Pose::ConstPtr &pose_message)
 
 double getTheta(double angle)
 {
-    //If theta is negative it is converted to the corresponding positive angle (Theta becomes negative when the turtle rotates clockwise).
-    double theta = angle < 0 ? angle + 2 * M_PI : angle;
-    //cout << "Got theta: " << theta << endl;
-    return theta;
+     //If theta is negative it is converted to the corresponding positive angle (Theta becomes negative when the turtle rotates clockwise).
+     double theta = angle < 0 ? angle + 2 * M_PI : angle;
+     //cout << "Got theta: " << theta << endl;
+     return theta;
 }
 
 //Converts degrees to radians.
@@ -280,50 +278,50 @@ point convertCoordinatesOfPoint(point Coord)
 
      //The coordinates of the found paper from the robots Origin point.
      //Determined from the coordinates of the robot from its Origin + the vector from the robot centre to the found point.
-     
+
      point paperPoint;
      paperPoint.x = 3 + rotatedPoint.x; //cur_pose.x
      paperPoint.y = 4 + rotatedPoint.y; //cur_pose.y
      return paperPoint;
-} 
+}
 
-visualization_msgs::Marker pointToMark( point markcalc )
+visualization_msgs::Marker pointToMark(point markcalc)
 {
-    visualization_msgs::Marker marker;
-    // Set the frame ID and timestamp.  See the TF tutorials for information on these.
-    marker.header.frame_id = "/my_frame";
-    marker.header.stamp = ros::Time();
+     visualization_msgs::Marker marker;
+     // Set the frame ID and timestamp.  See the TF tutorials for information on these.
+     marker.header.frame_id = "/odom";
+     marker.header.stamp = ros::Time();
 
-    // Set the namespace and id for this marker.  This serves to create a unique ID
-    // Any marker sent with the same namespace and id will overwrite the old one
-    marker.ns = "paper_pose";
-    marker.id = 0;
+     // Set the namespace and id for this marker.  This serves to create a unique ID
+     // Any marker sent with the same namespace and id will overwrite the old one
+     marker.ns = "paper_pose";
+     marker.id = 0;
 
-    // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
-    marker.type = visualization_msgs::Marker::CUBE;
+     // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
+     marker.type = visualization_msgs::Marker::CUBE;
 
-    // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
-    marker.action = visualization_msgs::Marker::ADD;
+     // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
+     marker.action = visualization_msgs::Marker::ADD;
 
-    // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
-    marker.pose.position.x = markcalc.x;
-    marker.pose.position.y = markcalc.y;
-    marker.pose.position.z = 0;
+     // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
+     marker.pose.position.x = markcalc.x;
+     marker.pose.position.y = markcalc.y;
+     marker.pose.position.z = 0;
 
-    marker.pose.orientation.w = 1.0;
-    // Set the scale of the marker -- 1x1x1 here means 1m on a side
-    marker.scale.x = 0.1;
-    marker.scale.y = 0.1;
-    marker.scale.z = 0.1;
+     marker.pose.orientation.w = 1.0;
+     // Set the scale of the marker -- 1x1x1 here means 1m on a side
+     marker.scale.x = 0.1;
+     marker.scale.y = 0.1;
+     marker.scale.z = 0.1;
 
-    // Set the color -- be sure to set alpha to something non-zero!
-    marker.color.r = 1.0;
-    marker.color.g = 0.0;
-    marker.color.b = 0.0;
-    marker.color.a = 1.0;
+     // Set the color -- be sure to set alpha to something non-zero!
+     marker.color.r = 1.0;
+     marker.color.g = 0.0;
+     marker.color.b = 0.0;
+     marker.color.a = 1.0;
 
-    marker.lifetime = ros::Duration(60);
+     marker.lifetime = ros::Duration(60);
 
-    iterationCount++;
-    return marker;
-  }
+     iterationCount++;
+     return marker;
+}
